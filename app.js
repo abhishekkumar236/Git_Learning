1b67bd9dc57b56463e9ace3a3a52fbdff6b3a153
abhishekkumar236 <abhishekkumar.ak23610@gmail.com>
2023-11-19 23:46:59 +0530

    adding dom files

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+let listItems = document.querySelectorAll(".list-group-item");
+
+listItems[2].style.backgroundColor = "green";
+
+listItems.forEach((element) => {
+  element.style.fontWeight = "bold";
+});
